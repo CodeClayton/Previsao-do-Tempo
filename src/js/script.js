6177e794c461b4b81afbcc7b02bea595da263880
Clayton Henrique <134659710+CodeClayton@users.noreply.github.com>
2024-01-15 19:14:39 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,3 @@
-let apiKey = prompt('Insira a chave API');
-//recebe a chave api (para não haver spam)
-
 const form = document.getElementById('form');
 
 form.addEventListener('submit', async (event) =>{
@@ -13,7 +10,7 @@ form.addEventListener('submit', async (event) =>{
        showWarning("Procurando a cidade....");
        
         
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=${apiKey}&units=metric&lang=pt-br`;
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=d2771a0d8ca543807127cae07746c481&units=metric&lang=pt-br`;
 
         let result = await fetch(url);
         //Faz um requisição a url acima para acessar a API.
